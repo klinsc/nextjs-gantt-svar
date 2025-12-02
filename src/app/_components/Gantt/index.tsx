303f9b6fbe296b6f28168a400cc5482c61f7341b
Chatbordin <klinscea@gmail.com>
2025-12-02 14:04:57 +0700

    Refactor GanttPage component: remove timeline calculations and datasource file

--- src/app/_components/Gantt/index.tsx
+++ src/app/_components/Gantt/index.tsx
@@ -3,20 +3,9 @@
 import { ContextMenu, Editor, Gantt, Willow } from "@svar-ui/react-gantt";
 import { useMemo, useState } from "react";
 import { getData } from "../../data";
-import { tasks } from "../../datasource";
 import "../../GanttZoom.css";
 
-const timelineStart = tasks.reduce(
-  (min, task) => (task.start < min ? task.start : min),
-  tasks[0].start
-);
-const timelineEnd = tasks.reduce(
-  (max, task) => (task.end > max ? task.end : max),
-  tasks[0].end
-);
-
 export default function GanttPage() {
-  const timeline = { start: timelineStart, end: timelineEnd };
   const [api, setApi] = useState<any>();
   const data = useMemo(() => getData(), []);
 
@@ -27,12 +16,6 @@ export default function GanttPage() {
           <h1>Svar React Gantt + Next.js Starter</h1>
           <p>
             App Router + Willow theme with sample resources and timeline scales.
-          </p>
-        </div>
-        <div>
-          <p>
-            <strong>Timeline:</strong> {timeline.start.toDateString()} →{" "}
-            {timeline.end.toDateString()}
           </p>
         </div>
       </div>
